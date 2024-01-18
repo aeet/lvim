6bb85016f19a19e2340ce449fea472a70a5c6376
aeet <atwoet@outlook.com>
2024-01-18 21:29:41 +0800

    fix: fix dress

--- lua/user/dress.lua
+++ lua/user/dress.lua
@@ -19,9 +19,14 @@ M.config = function()
     },
     select = {
       format_item_override = {
+        -- codeaction = function(action_tuple)
+        --   local title = action_tuple.action.title:gsub("\r\n", "\\r\\n")
+        --   local client = vim.lsp.get_client_by_id(action_tuple.ctx.client_id)
+        --   return string.format("%s\t[%s]", title:gsub("\n", "\\n"), client.name)
+        -- end,
         codeaction = function(action_tuple)
-          local title = action_tuple.action.title:gsub("\r\n", "\\r\\n")
-          local client = vim.lsp.get_client_by_id(action_tuple.ctx.client_id)
+          local title = action_tuple[2].title:gsub("\r\n", "\\r\\n")
+          local client = vim.lsp.get_client_by_id(action_tuple[1])
           return string.format("%s\t[%s]", title:gsub("\n", "\\n"), client.name)
         end,
       },
@@ -30,4 +35,3 @@ M.config = function()
 end
 
 return M
-
